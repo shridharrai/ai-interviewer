f8dda5f425dd46f5c4b52bff1046a001971fff6b
Priyank Pal <ppal@cb.careerbuilder.com>
2025-05-15 19:57:04 +0530

    UI changes done

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -5,12 +5,20 @@ import Image from "next/image";
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
     <div className="root-layout">
-      <nav>
+       <div className="flex w-screen bg-[#230839]">
+          <img
+            src="https://clarissa-ams-bucket.s3.ap-southeast-2.amazonaws.com/funlogos/floatingghostleft.png"
+            width={200}
+            height={80}
+          />
+        </div>
+      {/* <nav>
+       
         <Link href="/" className="flex items-center gap-2">
           <Image src="/logo.svg" alt="Logo" width={38} height={32} />
           <h2 className="text-primary-100">InterviewIQ</h2>
         </Link>
-      </nav>
+      </nav> */}
 
       {children}
     </div>
